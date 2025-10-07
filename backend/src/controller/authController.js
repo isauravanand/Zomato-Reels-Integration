fdfb41317305c2ffda1e599e2681a11f09d043a7
Saurav Anand <sauravanand1608@gmail.com>
2025-10-07 19:09:12 +0530

    Updated userController with food partner

--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -1,4 +1,5 @@
 const userModel = require("../models/user.model");
+const foodPartnerModel = require("../models/foodPartner.model");
 const bcrypt = require('bcrypt');
 const jwt = require("jsonwebtoken");
 
@@ -20,34 +21,34 @@ async function registerUser(req, res) {
         password: hashedPassword,
     })
 
-    const token = jwt.sign({id: user._id},
+    const token = jwt.sign({ id: user._id },
         process.env.JWT_SECRET
     )
 
-    res.cookie("token",token);
+    res.cookie("token", token);
     res.status(201).json({
-        message:"User Succesfully Registered",
-        user:{
-            id:user._id,
-            email:user.email,
-            fullname:user.fullname
+        message: "User Succesfully Registered",
+        user: {
+            _id: user._id,
+            email: user.email,
+            fullname: user.fullname
         }
     })
 }
 
-async function loginUser(req,res){
-    const {email,password} = req.body;
-    const user = await userModel.findOne({email});
+async function loginUser(req, res) {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
 
-    if(!user){
+    if (!user) {
         res.status(400).json({
-             message:"Invalid Email or Password"
+            message: "Invalid Email or Password"
         })
     }
 
-    const isPasswordValid = await bcrypt.compare(password,user.password);
+    const isPasswordValid = await bcrypt.compare(password, user.password);
 
-    if(!isPasswordValid){
+    if (!isPasswordValid) {
         res.status(400).json({
             message: "Invalid Email or Password"
         })
@@ -68,19 +69,95 @@ async function loginUser(req,res){
     })
 }
 
- 
-function logout(req,res){
+
+function logoutUser(req, res) {
     res.clearCookie("token");
     res.status(200).json({
-        message:"User Logged Out Successfully"
+        message: "User Logged Out Successfully"
     })
 }
 
+async function registerFoodPartner(req, res) {
+    const { email, name, password } = req.body;
+
+    const isUserAlreadyExists = await foodPartnerModel.findOne({ email });
+
+    if (isUserAlreadyExists) {
+        return res.send(400).json({
+            message: "Food Partner Account Already Exists"
+        })
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const foodPartner = await foodPartnerModel.create({
+        name,
+        email,
+        password: hashedPassword,
+    });
+
+    const token = jwt.sign({ id: foodPartner._id },
+        process.env.JWT_SECRET
+    )
+
+    res.cookie("token", token);
+    res.status(201).json({
+        message: "Food Partner Account Registered Succesfully",
+        foodPartner: {
+            _id: foodPartner._id,
+            email: foodPartner.email,
+            name: foodPartner.name
+        }
+    })
 
 
+}
 
-module.exports ={
+async function loginFoodPartner(req,res){
+    const { email, password } = req.body;
+    const foodPartner = await foodPartnerModel.findOne({ email });
+
+    if (!foodPartner) {
+        res.status(400).json({
+            message: "Invalid Email or Password"
+        })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, foodPartner.password);
+
+    if (!isPasswordValid) {
+        res.status(400).json({
+            message: "Invalid Email or Password"
+        })
+    }
+
+    const token = jwt.sign({ id: foodPartner._id },
+        process.env.JWT_SECRET
+    )
+
+    res.cookie("token", token);
+    res.status(200).json({
+        message: "Food Partner Logged in Successfully !",
+        user: {
+            id: foodPartner._id,
+            email: foodPartner.email,
+            name:foodPartner.name
+        }
+    })
+}
+
+function logoutFoodPartner(req,res){
+    res.clearCookie("token");
+    res.status(200).json({
+        message: "Food Partner Logged Out Successfully"
+    })
+}
+
+module.exports = {
     registerUser,
     loginUser,
-    logout
+    logoutUser,
+    registerFoodPartner,
+    loginFoodPartner,
+    logoutFoodPartner,
 }

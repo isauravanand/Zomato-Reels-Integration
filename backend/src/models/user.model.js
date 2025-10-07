d5344ba00ca4739aeedc07b530044c094ef429f7
Saurav Anand <sauravanand1608@gmail.com>
2025-10-07 19:10:53 +0530

    updated user model with food partner

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     fullname:{
         type:String,
-        require:true,
+        required:true,
     },
     email:{
         type:String,
-        require:true,
+        required:true,
         unique:true,
     },
     password:{

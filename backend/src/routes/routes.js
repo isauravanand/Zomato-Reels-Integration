e673ac96c73584056c47365929929ea46e3c7b0f
Saurav Anand <sauravanand1608@gmail.com>
2025-10-07 19:17:16 +0530

    adding documentation for routes

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router = express.Router();
 const authController =  require("../controller/authController");
 
+//user auth Apis
 router.post("/user/register",authController.registerUser);
 router.post("/user/login",authController.loginUser);
 router.get("/user/logout",authController.logoutUser);
+
+//Food partner Apis
 router.post("/food-partner/register",authController.registerFoodPartner);
 router.post("/food-partner/login",authController.loginFoodPartner);
 router.get("/food-partner/logout",authController.logoutFoodPartner);

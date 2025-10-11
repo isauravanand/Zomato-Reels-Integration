4769416b266fcfc06eea93533fbcfdb6e470f0df
Saurav Anand <sauravanand1608@gmail.com>
2025-10-11 23:48:09 +0530

    modified food partner model

--- backend/src/models/foodPartner.model.js
+++ backend/src/models/foodPartner.model.js
@@ -1,10 +1,21 @@
 const mongoose = require("mongoose");
 
 const foodPartnerSchema = new mongoose.Schema({
-
     name:{
         type:String,
         required:true,
+    },
+    contactName:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:String,
+        required:true,
+    },
+    address:{
+        type:String,
+        required:true
     },
     email:{
         type:String,

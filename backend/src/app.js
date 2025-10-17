363910ba7d4323ceb8cf936a92bd0ab6a9d8f152
Saurav Anand <sauravanand1608@gmail.com>
2025-10-17 11:29:15 +0530

    installed more packags

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,11 +4,15 @@ const cookieparser = require("cookie-parser");
 const app = express();
 const authRoutes = require("./routes/auth");
 const foodRoutes = require("./routes/food");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieparser());
-
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}));
 
 
 app.get("/",(req,res)=>{

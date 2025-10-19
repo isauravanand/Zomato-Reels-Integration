126bde870bd6baf7ca76b5ee83b66f7de2804d08
Saurav Anand <sauravanand1608@gmail.com>
2025-10-19 21:07:58 +0530

    Added Food Partner controller

--- backend/src/controller/foodPartnerController.js
+++ backend/src/controller/foodPartnerController.js
@@ -1,10 +1,13 @@
 const foodPartnerModel = require("../models/foodPartner.model");
+const foodModel = require("../models/fooditem.model");
 
 async function getFoodPartnerById(req, res) {
     const foodPartnerId = req.params.id;
     // console.log("foodpartner id is " + foodPartnerId);
 
     const foodPartner = await foodPartnerModel.findById(foodPartnerId);
+    const foodItemsByFoodpartner = await foodModel.find({foodpartner:foodPartnerId});
+
     if (!foodPartner) {
         return res.status(404).json({
             message: "Food Partner not Found"
@@ -12,7 +15,10 @@ async function getFoodPartnerById(req, res) {
     }
     res.status(200).json({
         message: "Food Partner Found SucessFully",
-        foodPartner
+        foodPartner: {
+            ...foodPartner.toObject(),
+            foodItems:foodItemsByFoodpartner
+        }
     });
 }
 
